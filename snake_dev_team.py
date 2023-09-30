7bb785b2db766bd8a8ce428d94b27275d32218f3
Princeps Polaris <polycapn@gmail.com>
2023-09-30 04:34:09 -0700

    updated the packages to the correct one

--- snake_dev_team.py
+++ snake_dev_team.py
@@ -1,7 +1,7 @@
-from flaml import autogen
+from autogen import AssistantAgent, UserProxyAgent, config_list_from_json, GroupChat, GroupChatManager
 
 # Load the configuration for GPT-4 from a JSON file
-config_list_gpt4 = autogen.config_list_from_json(
+config_list_gpt4 = config_list_from_json(
     "../OAI_CONFIG_LIST.json",
     filter_dict={
         "model": ["gpt-4-0613, gpt-4-32k, gpt-4, gpt-4-0314"],
@@ -20,7 +20,7 @@ gpt4_config = {
 working_directory = "game_files"
 
 # Initialize the Player agent, responsible for providing gameplay feedback
-player = autogen.UserProxyAgent(
+player = UserProxyAgent(
     name="Player",
     system_message="Player: Your role is to provide feedback on the gameplay. Collaborate with the Game Designer to ensure the game meets desired expectations.",
     code_execution_config={
@@ -32,21 +32,21 @@ player = autogen.UserProxyAgent(
 )
 
 # Initialize the Game Designer agent, responsible for designing the game
-game_designer = autogen.AssistantAgent(
+game_designer = AssistantAgent(
     name="Game_Designer",
     llm_config=gpt4_config,
     system_message="Game Designer: Design the snake game, ensuring all details are documented in 'game_design.txt'. Collaborate with the Player to align the design with feedback and expectations."
 )
 
 # Initialize the Programmer agent, responsible for coding the game
-programmer = autogen.AssistantAgent(
+programmer = AssistantAgent(
     name="Programmer",
     llm_config=gpt4_config,
     system_message="Programmer: Code the snake game and save it in the working directory. For code execution, collaborate with the Code Executor. If feedback is needed, consult the Game Tester."
 )
 
 # Initialize the Game Tester agent, responsible for playtesting the game
-game_tester = autogen.UserProxyAgent(
+game_tester = UserProxyAgent(
     name="Game_Tester",
     system_message="Game Tester: Playtest the game and provide feedback on gameplay mechanics and user experience. Report any bugs or glitches. Collaborate with the Programmer for any necessary adjustments.",
     code_execution_config={
@@ -59,7 +59,7 @@ game_tester = autogen.UserProxyAgent(
 )
 
 # Initialize the Code Executor agent, responsible for executing the game code
-code_executor = autogen.UserProxyAgent(
+code_executor = UserProxyAgent(
     name="Code_Executor",
     system_message="Code Executor: Execute the provided code from the Programmer in the designated environment. Report outcomes and potential issues. Ensure the code follows best practices and recommend enhancements to the Programmer.",
     code_execution_config={
@@ -72,14 +72,14 @@ code_executor = autogen.UserProxyAgent(
 )
 
 # Set up the group chat with all the agents
-groupchat = autogen.GroupChat(
+groupchat = GroupChat(
     agents=[player, game_tester, game_designer, programmer, code_executor],
     messages=[],
     max_round=150
 )
 
 # Create a manager for the group chat using the GPT-4 configuration
-manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=gpt4_config)
+manager = GroupChatManager(groupchat=groupchat, llm_config=gpt4_config)
 
 # Start the conversation with the Player's message
 player.initiate_chat(
